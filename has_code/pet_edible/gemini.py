77c677ee37516d3741741f2765ad9c6fbbe93fa8
Anish Taluri <anisht@outlook.com>
2024-04-21 04:22:40 -0700

    Update gemini.py
    -Added more precise examples for accurate depictions

--- has_code/pet_edible/gemini.py
+++ has_code/pet_edible/gemini.py
@@ -41,8 +41,8 @@ def gemini(model_prediction, ImageToParse):
 
     img = ImageToParse
 
-    prompt = f"""Tell me if the image is {model_prediction} and it is something edible for a dog and store it into isEdible.
-    Give me the good or bad reasons (dependent on isEdible) and store it into reason.
+    prompt = f"""Tell me if the image is {model_prediction} and it is something edible for a dog and store it into isEdible. Do NOT store as a string or any other data type, only as a boolean.
+    Give me the good or bad reasons (dependent on isEdible) and store it into reason. If it is a popular brand, say what the food item is. For example, if it's an Oreo, say that it is.
     Classify the severity of the food and store it into severity. Should be between Most Severe, Medium Severity, Moderate, Healthy.
     Output a JSON file with the format of {{'isEdible': boolean, 'reason': string, 'severity': string}}
     An example: {{'isEdible': False, 'reason': 'Chocolate is bad for dogs.', 'severity': 'Most Severe'}}"""
